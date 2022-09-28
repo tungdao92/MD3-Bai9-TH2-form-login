1bc7799fc85c610b42d71ace6a49b2d093f565a7
unknown <tungdao923@gmail.com>
2022-09-28 20:31:12 +0700

    up

---
+++ src/main/java/com/example/bai9th2formlogin/HelloServlet.java
@@ -0,0 +1,45 @@
+package com.example.bai9th2formlogin;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+
+
+    public void destroy() {
+    }
+}
